65731092d3c5983e0b981f451e7141570194a03d
Hassan Fida <hassansfida@gmail.com>
2024-03-25 01:10:54 +0500

    initial server basic covered

--- web-server/index.js
+++ web-server/index.js
@@ -1,12 +1,49 @@
 const http = require('http')
 const url = require('url')
+const fs = require('fs')
+
+const productsArrray = (tempcard, product) => {
+    let output = tempcard.replace(/{%PRODUCT_NAME%}/g, product.title)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%RATING%}/g, product.rating)
+    output = output.replace(/{%IMAGE%}/g, product.images[0])
+    output = output.replace(/{%ID%}/g, product.id)
+    return output
+}
+
+const data = fs.readFileSync(`${__dirname}/productsData/products.json`, 'utf-8')
+const homePage = fs.readFileSync(`${__dirname}/templates/homePage.html`, 'utf-8')
+const tempcard = fs.readFileSync(`${__dirname}/templates/tempcard.html`, 'utf-8')
+const singleProduct = fs.readFileSync(`${__dirname}/templates/singleProduct.html`, 'utf-8')
+
+const dataobj = JSON.parse(data)
+
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url
-    if(pathName == '/' || pathName == '/overview'){
-        res.end("This is Overview")
-    } else if (pathName == '/product') {
-        res.end("This is Product")
+    // const pathName = req.url
+    const { query, pathname } = url.parse(req.url, true)
+    
+    if(pathname == '/' || pathname == '/overview'){
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        })
+        const readJsonData = dataobj.products.map(el => productsArrray(tempcard, el)).join('')
+        const indexPage = homePage.replace(/{%PRODUCT_LISTENING%}/g, readJsonData)
+        res.end(indexPage)
+    } else if (pathname == '/product') {
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        })
+        const product = dataobj.products[query.id]
+        const output = productsArrray(singleProduct, product)
+        const singleProductPage = homePage.replace(/{%PRODUCT_LISTENING%}/g, output)
+        res.end(singleProductPage)
+    } else if (pathname == '/api') {
+        res.writeHead(200, {
+            'content-type': 'application/json'
+        })
+        res.end(data)
     } else {
         res.writeHead(404, {
             'content-type': 'text/html'
