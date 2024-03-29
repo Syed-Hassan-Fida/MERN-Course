dfd3443f515d5d8a20e1eb1bb7147a7db33ba809
Hassan Fida <hassansfida@gmail.com>
2024-03-30 02:24:04 +0500

    patch request added

--- mini-project/app.js
+++ mini-project/app.js
@@ -8,6 +8,7 @@ app.use(express.json())
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
+// get all toyrs data
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: "success",
@@ -18,6 +19,27 @@ app.get('/api/v1/tours', (req, res) => {
     })
 })
 
+// get single tour data
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1 // type conversion
+    const singleTour = tours.find( el => el.id === id )
+
+    if(!singleTour){
+        res.status(200).json({
+            status: "fail",
+            message: "INVALID ID"
+        })
+    } else {
+        res.status(200).json({
+            status: "success",
+            data: {
+                singleTour
+            }
+        })
+    }
+})
+
+// add tour data
 app.post('/api/v1/add-tour', (req, res) => {
     const formData = req.body
     const newId = tours[tours.length - 1].id + 1
@@ -43,6 +65,25 @@ app.post('/api/v1/add-tour', (req, res) => {
 
     })
 })
+
+// update tour
+app.patch('/api/v1/tour/:id/update', (req, res) => {
+
+    const id = req.params.id * 1 // type conversion
+    if(id > tours.length){
+        res.status(200).json({
+            status: "fail",
+            message: "INVALID ID"
+        })
+    } else {
+        res.status(200).json({
+            status: "success",
+            data: {
+                tour: req.body
+            }
+        })
+    }
+}) 
 
 app.listen(port, '127.0.0.1', () => {
     console.log("http://127.0.0.1:8000")
