62878e03b1032fc1fb1eccb3eabdd327aae2c863
Hassan Fida <hassansfida@gmail.com>
2024-04-03 21:28:01 +0500

    configer ESLINT

--- mini-project/app.js
+++ mini-project/app.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
 
 if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
-}
+    app.use(morgan('dev'));
+};
 
-app.use(express.json())
-app.use(express.static(`${__dirname}/public`))
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 const tourRouters = require('./routes/tourRouters')
 const userRouters = require('./routes/userRouters')
