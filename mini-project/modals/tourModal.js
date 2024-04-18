13da1952b65b3461b9c3891cbec904798faa8879
Hassan Fida <hassansfida@gmail.com>
2024-04-18 23:59:31 +0500

    updated tours model

--- mini-project/modals/tourModal.js
+++ mini-project/modals/tourModal.js
@@ -3,15 +3,54 @@ const tourScheme = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Must Have Tour Name'],
-        unique: true
+        unique: true,
+        trim: true
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, 'Tour must Has a duration...']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'Tour must Has a group size...']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'Tour must Has a group difficulty...']
+    },
+    ratingsAverage: {
         type: Number,
         default: 2.5
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
     },
     price: {
         type: Number,
         required: [true, 'Tour must Has a price...']
+    },
+    priceDiscount: {
+        type: Number
+    },
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'Tour must Has a summary...']
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'Tour must has a image...']
+    },
+    images: [String],
+    startDates: [Date],
+    createdAt: {
+        type: Date,
+        default: Date.now()
     }
 })
 
